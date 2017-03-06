d825af63f6f5845dbb1fc2174d762fbc0bab21d0
Sean Wu <wuxuewen_hz@163.com>
2017-03-06 22:31:13 +0800

    add Some TestCode

---
+++ demoCode/MemoryTest/HeapOOM.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * VM Args: -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * Created by SeanWu on 2017/3/6.
+ */
+public class HeapOOM {
+
+    /**
+     * this class will result OutOfMemoryError : Java heap space
+     */
+    static class TestModel {
+    }
+
+    /**
+     * this class will result OutOfMemoryError : GC overhead limit exceeded
+     */
+    static class TestModel2 {
+        double d = 1.1;
+    }
+    public static void main(String[] args) {
+
+        List<TestModel2> list = new ArrayList<>();
+        while (true){
+            list.add(new TestModel2());
+        }
+    }
+}
